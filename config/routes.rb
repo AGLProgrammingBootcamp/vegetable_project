6c8f4102de83eda401fad9739a2635f6b23df860
tatsuyamatsubara <matsubara.t.ai@m.titech.ac.jp>
2016-03-26 05:57:05 +0000

    Modify belikebutton

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :agriusers
-
+  resources :agriusers do
+    member do
+      get 'be_liked'
+    end
+  end
+  
   root 'home#top'
   get '/about' => 'home#about'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
