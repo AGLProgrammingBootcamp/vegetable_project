e2eb4eb51495f5231c5e5870fbb2ab19157bd5ab
t_kambara <t_kambara_renrakusaki@yahoo.co.jp>
2016-03-26 03:00:33 +0000

    Modify_Login

--- app/models/agriuser.rb
+++ app/models/agriuser.rb
@@ -1,4 +1,8 @@
 class Agriuser < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
     validates:name, presence:true
     validates:email, presence:true,uniqueness:true
     def self.authenticate(name, email)
